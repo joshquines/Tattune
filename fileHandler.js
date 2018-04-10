e070363a1f3b611f237e0b0ac54696446e7bd682
Josh <josh.qns@gmail.com>
2018-04-10 14:26:10 -0600

    some stuff

--- fileHandler.js
+++ fileHandler.js
@@ -1,67 +1,98 @@
-import html2canvas from 'html2canvas';
-
 // FUNCTIONS 
-let wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    waveColor: 'red',
-    progressColor: 'purple'
-  });
 
 // UPLOAD THE AUDIO FILE HERE
+
+function uploadFile(){
+
+    let wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        waveColor: 'red',
+        progressColor: 'purple'
+      });
+    let txt = "";
+
     // Taken from: https://www.w3schools.com/jsref/prop_fileupload_files.asp
-    let audioFile = document.getElementById("audioFile");
+    let audioFile = document.getElementById("audioFileUpload");
+    document.getElementById("uploadResponse").innerHTML = txt;
+    txt += "I uploaded stuff2";
+    document.getElementById("uploadResponse").innerHTML = txt;
 
     // Limit File Size
-    if ('files' in x) {
-        if (x.files.length == 0) {
+    txt += "I'm bout to do shit";
+    document.getElementById("uploadResponse").innerHTML = txt;
+    let fileType = audioFile.files[0].type;
+
+
+    if ('files' in audioFile) {
+        if (audioFile.files.length == 0) {
             txt = "Select one or more files.";
-            document.getElementById ("uploadResponse").innerHTML = txt;
+            document.getElementById("uploadResponse").innerHTML = "Select one or more files.";
             // Terminate script, Redirect
 
         } else if (audioFile.files.length > 1){
             txt = "Please only select 1 file";
-            document.getElementById ("uploadResponse").innerHTML = txt;
+            document.getElementById("uploadResponse").innerHTML = "Please only select 1 file";
             // Terminate Script, Redirect
 
-        } else {
-            for (var i = 0; i < audioFile.files.length; i++) {
+        } else if (fileType != fileType.indexOf('audio') != -1){
+            document.getElementById("uploadResponse").innerHTML = "Needs to be audio bitch";
+        }else {
+            for (let i = 0; i < audioFile.files.length; i++) {
                 txt += "<br><strong>" + (i+1) + ". file</strong><br>";
-                var file = audioFile.files[i];
+                let file = audioFile.files[i];
                 if ('name' in file) {
                     // txt += "name: " + file.name + "<br>";
                     fileName = file.name;
+                    txt += fileName;
+                    document.getElementById("uploadResponse").innerHTML = txt;
                 }
                 if ('size' in file) {
 
                     // Check if filesize over 10MB 
-                    if (file.size > 10000){
+                    if (file.size > 10000000){
                         txt += "Filesize: " + file.size + " bytes is too large. <br>";
-                        document.getElementById ("uploadResponse").innerHTML = txt;
+                        document.getElementById("uploadResponse").innerHTML = txt;
                         // Terminate Script, Redirect
 
                     } 
                     else{
                         txt += "Upload of " + fileName + " was successful";
+                        document.getElementById ("uploadResponse").innerHTML = "Upload of " + fileName + " was successful";
+
+                        // RENDER THE AUDIO FILE HERE
+                        wavesurfer.load(audioFile);
+                        //document.getElementById ("imageText").innerHTML = waveformImage;
+
+                        /*
+                        // Download Waveform Button
+                        // Container is #waveform
+                        let waveformImage = html2canvas(document.querySelector("#waveform")).then(canvas => {
+                            document.body.appendChild(canvas)
+                        });
+
+                        document.getElementById ("imageText").innerHTML = waveformImage; */
                     }
                 }
             }
         }
     } 
+}
 
-    // Send response message to uploadResponse div or something. 
-    document.getElementById ("uploadResponse").innerHTML = txt;
+
 
 
 // RENDER THE AUDIO FILE HERE
+/*
 wavesurfer.load(audioFile);
-
     // Download Waveform Button
     // Container is #waveform
     let waveformImage = html2canvas(document.querySelector("#waveform")).then(canvas => {
         document.body.appendChild(canvas)
     });
     
-    document.getElementById ("imageText").innerHTML = waveformImage;
+    document.getElementById ("imageText").innerHTML = waveformImage; */
+    
+   
     
 
 //  THIS PART WILL PROBABLY JUST BE SPLIT INTO DIFFERENT FILES DEPENDING ON WHAT THE USER CHOOSES 
