f0d56eba51ada3623fc3829199f83ae763a37036
Josh <josh.qns@gmail.com>
2018-04-14 11:42:27 -0600

    asdfg

--- fileHandler.js
+++ fileHandler.js
@@ -1,38 +1,51 @@
 // FUNCTIONS 
 
-// UPLOAD THE AUDIO FILE HERE
+function loadWave() {
+    document.getElementById("op").innerHTML='<object type="text/html" data="fileHandlerTest.html" ></object>';
+}
 
 function download(){
-    let a = document.body.appendChild(
-        document.createElement("a")
-    );
 
-    let audioFile = document.getElementById("audioFileUpload");
-    for (let i = 0; i < audioFile.files.length; i++) {
-        let file = audioFile.files[i];
-        if ('name' in file) {
-            fileName = file.name;
-            fileName = fileName.slice(0, -4)
-    a.download = fileName;
-    let elements = document.getElementById("waveform").children;
-    let txt = "";
-    for (let i = 0; i < elements.length; i++){
-        txt += "counter: " + i + elements[i] + "<br>";
-        document.getElementById("yo").innerHTML = txt;
-    }
-    a.href = "data:text/html," + document.getElementById("waveform").children;
-    a.click();
-        }
-    }
+    let waveimg = document.getElementById("waveform");
+  
+    let img = null;
+  
+    let imageFile = html2canvas(waveimg,{
+       allowTaint: true
+    }).then(canvas => {
+        console.log("html2canvas");
+        imgURL = canvas.toDataURL("image/png");
+        img = document.createElement("img");
+        img.src = imgURL;
+        document.body.appendChild(img);
+  
+      let audioFile = document.getElementById("audioFileUpload");
+      for (let i = 0; i < audioFile.files.length; i++) {
+          let file = audioFile.files[i];
+          if ('name' in file) {
+              fileName = file.name;
+              fileName = fileName.slice(0, -4)
+          }
+      }
+  
+      let a = document.body.appendChild(
+        document.createElement("a")
+      );
+      a.download = fileName;
+      a.href = img.src;
+      a.click();
+    });
 }
 
 function uploadFile(){
 
     let wavesurfer = WaveSurfer.create({
         container: '#waveform',
-        waveColor: 'red',
+        waveColor: 'green',
         progressColor: 'purple',
-        responsive: 'true',
+        responsive: 'false',
+        barWidth: '1',
+        scrollParent: 'true'
       });
     let waver = wavesurfer;
     let txt = "";
@@ -86,40 +99,27 @@ function uploadFile(){
                     } 
                     else{
 
-                        // 
-
                         txt += "Upload of " + fileName + " was successful";
                         txt += audioFile;
+
+                        // Success Message to HTML Page
                         document.getElementById ("uploadResponse").innerHTML = txt;
 
                         // RENDER THE AUDIO FILE HERE
-
                         // This Reads the File
                         let reader = new FileReader();
-                        //reader.onload = loadedCallback;
-                        //let fileContent = reader.readAsDataURL(audioFile);
                         let fileContent = URL.createObjectURL(audioFile.files[0]);
 
                         // This creates the waveform
                         let waveformImage = wavesurfer.load(fileContent);
 
-
-                        let elements = document.getElementById("waveform").children;
-                        let txts = "";
-                        for (let i = 0; i < elements.length; i++){
-                            txt += "counter: " + i + elements[i] + "<br>";
-                            document.getElementById("yo").innerHTML = txts;
-                        }
-
-
-
                         // Download Waveform Button
                         // Container is #waveform
-                        let imageFile = html2canvas(document.querySelector("#waveform")).then(canvas => {
+                        let imageFile = html2canvas(document.querySelector("div > wave > canvas")).then(canvas => {
                             document.body.appendChild(canvas)
                         });
 
-
+                        //loadWave();
                     }
                 }
             }
